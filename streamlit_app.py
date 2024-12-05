3f1dcf3db04d155632407884120be4a87bd5a9d8
Philipp Schrittwieser <123021085+Philipp-Schrittwieser@users.noreply.github.com>
2024-12-05 21:40:11 +0000

    w

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 import streamlit as st
 
 st.title("🎈 My new app")
+st.write("Nein ich kann es nicht glauben!?")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+st.write("Klappt das?")
+
+st.header("Wie geil ist das denn eigentlich?")
